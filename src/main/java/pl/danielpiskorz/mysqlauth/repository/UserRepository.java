677fa34f240e9d8440b63e05b4bf88ba333c480e
DanielPiskorz <eel5426@gmail.com>
2018-09-10 20:45:30 +0200

    User details service

--- src/main/java/pl/danielpiskorz/mysqlauth/repository/UserRepository.java
+++ src/main/java/pl/danielpiskorz/mysqlauth/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package pl.danielpiskorz.mysqlauth.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.danielpiskorz.mysqlauth.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 	
-
+	 Optional<User> findByName(String username);
 }
--- src/main/java/pl/danielpiskorz/mysqlauth/service/CustomUserDetailsService.java
+++ src/main/java/pl/danielpiskorz/mysqlauth/service/CustomUserDetailsService.java
@@ -1,12 +1,30 @@
 package pl.danielpiskorz.mysqlauth.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import pl.danielpiskorz.mysqlauth.model.CustomUserDetails;
+import pl.danielpiskorz.mysqlauth.model.User;
+import pl.danielpiskorz.mysqlauth.repository.UserRepository;
 
 public class CustomUserDetailsService implements UserDetailsService{
 
-		@Override
-		public UserDetails loadUserByUsername(String username){
-			return null;
-		}
+	
+	@Autowired
+	private UserRepository usersRepository;
+	
+	
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUsers = usersRepository.findByName(username);
+
+        optionalUsers
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return optionalUsers
+                .map(CustomUserDetails::new).get();
+    }
 }
