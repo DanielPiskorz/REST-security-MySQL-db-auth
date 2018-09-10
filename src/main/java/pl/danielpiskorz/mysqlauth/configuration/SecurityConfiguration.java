a7c6be663f12025d3c0bb303a38e83f783dc4fcd
DanielPiskorz <eel5426@gmail.com>
2018-09-10 20:54:09 +0200

    Pre authorization on controller

--- src/main/java/pl/danielpiskorz/mysqlauth/configuration/SecurityConfiguration.java
+++ src/main/java/pl/danielpiskorz/mysqlauth/configuration/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.danielpiskorz.mysqlauth.repository.UserRepository;
 import pl.danielpiskorz.mysqlauth.service.CustomUserDetailsService;
 
+@EnableGlobalMethodSecurity
 @EnableWebSecurity
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
 @Configuration
@@ -33,7 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("**/secured/**").authenticated()
+                .antMatchers("**/secured").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin().permitAll();
--- src/main/java/pl/danielpiskorz/mysqlauth/web/MainController.java
+++ src/main/java/pl/danielpiskorz/mysqlauth/web/MainController.java
@@ -1,5 +1,6 @@
 package pl.danielpiskorz.mysqlauth.web;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class MainController {
 			return "hello";
 		}
 		
+		@PreAuthorize("hasAnyRole('ADMIN')")
 		@GetMapping("/secured")
 		public String getSecuredResources(){
 			return "secured data";
